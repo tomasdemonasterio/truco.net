1d23a04a958dfcd0a5aaacef14460e655dacb7fb
Sofía de Monasterio <38118995+sofiademonasterio@users.noreply.github.com>
2024-05-08 17:56:52 -0300

    Update Player class to include player state and available actions

--- truco/Player.cs
+++ truco/Player.cs
@@ -2,98 +2,65 @@ namespace PlayerNS {
     using HandNS;
     using GameNS;
 
+    enum PlayerState {
+        isPlayingTruco,
+        isPlayingEnvido,
+        isPlayingRealEnvido,
+        isPlayingRetruco,
+        isPlayingValeCuatro,
+        isPlayingFaltaEnvido,
+        isPlayingIrseAlMazo,
+        isPlayingQuiero,
+        isPlayingPasar,
+        isPlayingStart
+    }
+
+    enum Acciones {
+        Truco,
+        Envido,
+        RealEnvido,
+        Retruco,
+        ValeCuatro,
+        FaltaEnvido,
+        IrseAlMazo,
+        Quiero,
+        Pasar}
+
     class Player {
-        private string name;
-        private Hand hand;
-        private int score;
-        private bool playerShowedCards;
+       
         private Game game;
-        
+        public string name { get;}
+        public Hand hand { get;}
+        public int score { get; set; }
+        public bool isFirstWinner { get; set; }
+        public PlayerState playerState { get; set; }
+        public int phasesWon { get; set; }
+        public HashSet<Acciones> availableActions { get; set; }
 
         public Player(string name, Game game) {
             this.name = name;
-            this.hand = new Hand();
-            this.score = 0;
-            this.playerShowedCards = false;
             this.game = game;
+            hand = new Hand();
+            score = 0;
+            isFirstWinner = false;
+            playerState = PlayerState.isPlayingStart;
+            phasesWon = 0;
+            availableActions = new HashSet<Acciones>() { Acciones.Truco, Acciones.Envido, Acciones.Pasar, Acciones.IrseAlMazo };
         }
 
-        public string GetName() {
-            return this.name;
-        }
-
-        public Hand GetHand() {
-            return this.hand;
-        }
-
-        public int GetScore() {
-            return this.score;
-        }
-
-
-        public void SetScore(int score) {
-            this.score = score;
-        }
-
-        public void SetHand(Hand hand) {
-            this.hand = hand;
-        }
-
-        public void truco() {
-            // Truco
-            if (playerShowedCards) {
-                game.round.roundScore = 2;
-            }
-            game.round.roundScore = 1;
-            game.round.roundState = RoundNS.RoundState.Truco;
-        }
-
-        public void quiero() {
-            // Quiero
-            game.round.roundScore = 2;
-            game.round.roundState = RoundNS.RoundState.Quiero;
-        }
-
-        public void retruco() {
-            // Retruco
-            game.round.roundScore = 3;
-            game.round.roundState = RoundNS.RoundState.Retruco;
-        }
-
-        public void valeCuatro() {
-            // Vale Cuatro
-            game.round.roundScore = 4;
-            game.round.roundState = RoundNS.RoundState.ValeCuatro;
-        }
-
-        public void envido() {
-            // Envido
-            game.round.roundState = RoundNS.RoundState.Envido;
-        }
-
-        public void realEnvido() {
-            // Real Envido
-            game.round.roundState = RoundNS.RoundState.RealEnvido;
-        }
-
-        public void faltaEnvido() {
-            // Falta Envido
-            game.round.roundState = RoundNS.RoundState.FaltaEnvido;
-        }
-
-        public void pasar(int index) {
-            // Pasar
+        public void playCard(int index) {
+            // Play card
+            game.round.playedCard(this, hand.GetCard(index));
             hand.removeCard(index);
         }
 
-        public void irseAlMazo() {
-            // Ir al mazo
+        public string playerAvailableActionsToString() {
+            string actions = "";
+            foreach (Acciones action in availableActions) {
+                actions += action.ToString() + " ";
+            }
+            return actions;
         }
-
-        public void faltaTruco() {
-            // Falta Truco
-            game.round.roundState = RoundNS.RoundState.FaltaTruco;
-        }
-
+        
     }
 }
